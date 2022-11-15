8da5b3f6db7e23f5d8d3a36e3a1d90d5732f471a
Ragnar <ragnar.naruski@voco.ee>
2022-11-15 11:35:10 +0200

    getter and setter

--- app.js
+++ app.js
@@ -2,3 +2,5 @@
 const mario=new Char('Mario')
 
 mario.Who()
+mario.setAge(25)
+console.log(mario.getAge())
--- char.js
+++ char.js
@@ -1,9 +1,17 @@
 class Char{
     constructor(name) {
         this.name=name
+        this.age
     }
 
     Who(){
         console.log("Hi, I'm "+this.name+'!')
+    }
+
+    setAge(age){
+        this.age=age
+    }
+    getAge(){
+        return this.age
     }
 }
